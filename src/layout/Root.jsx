9bb436ae0e650c78a19bf282f0869c633e3ba9ad
arian2004 <arian2004feni@gmail.com>
2025-07-08 03:54:07 +0600

    create & setup footer in rootLayout

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/Common/Navbar';
+import Footer from '../components/Common/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
